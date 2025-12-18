bd6d19feb341ccde6ebc99d4cf602acc70b156f1
Mohamed <mohamed.goudjil@etu.univ-amu.fr>
2025-12-18 18:36:34 +0100

    fix

--- envoie_avis.php
+++ envoie_avis.php
@@ -2,24 +2,18 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pseudo = htmlspecialchars($_POST['pseudo']);
     $message = htmlspecialchars($_POST['message']);
-    $to = "toimomo841@gmail.com"; 
 
+    // --- CETTE PARTIE SAUVEGARDE L'AVIS SUR TON SITE ---
+    $ligne = "Date: " . date('d-m-Y H:i') . " | Nom: $pseudo | Message: $message" . PHP_EOL;
+    file_put_contents('avis_pirates.txt', $ligne, FILE_APPEND);
+
+    // Tentative d'envoi mail (même si c'est bloqué, le fichier au-dessus sera créé)
+    $to = "toimomo845@gmail.com";
     $subject = "Nouvel avis de : $pseudo";
-    
-    $headers = "From: formulaire@encyclopedieonepiece.alwaysdata.net" . "\r\n" .
-               "Reply-To: " . $to . "\r\n" .
-               "Content-Type: text/plain; charset=utf-8" . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
+    $headers = "From: formulaire@encyclopedieonepiece.alwaysdata.net";
+    mail($to, $subject, $message, $headers);
 
-    $content = "Moussaillon, tu as reçu un message !\n\nNom : $pseudo\nMessage : $message";
-
-    // --- CETTE LIGNE EST TA ROUE DE SECOURS ---
-    // Elle écrit l'avis dans un fichier avis.txt sur ton serveur
-    file_put_contents('test_avis.txt', "Pseudo: $pseudo - Message: $message\n", FILE_APPEND);
-    if(mail($to, $subject, $content, $headers)) {
-        header("Location: index.html?envoi=reussi");
-    } else {
-        echo "Erreur d'envoi mail, mais l'avis a été sauvegardé dans avis.txt.";
-    }
+    // Redirection
+    header("Location: index.html?envoi=reussi");
 }
 ?>
